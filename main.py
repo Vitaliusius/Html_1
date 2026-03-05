b5a714413306ba2d81818bff0bf1efe8f0f0b771
Vitaliy <vitalianobro@gmail.com>
2026-03-05 12:59:40 +0300

    Update main.py
    Убрал функцию получения названия напитков
    Добавил метод sorted_categories.items()
    Изменил название переменной CREATION_YEAR
    Добавил переменную окружения PATH_TABLE
    Изменил переменную old на age

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import datetime
 import collections
 import pandas
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
 
+from dotenv import load_dotenv
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+CREATION_YEAR = 1920
 
 
 def get_an_age(year_of_creation):
     this_year = datetime.date.today()
-    age_winery = this_year.year - year_of_creation
+    age_winery = this_year.year - CREATION_YEAR
 
     return age_winery
-
-
-def get_category_titles(sorted_categories):
-    titles = [ drink for drink in sorted_categories]
-
-    return titles
 
 
 def get_sorting_categories(all_drinks):
@@ -41,20 +40,20 @@ def get_age_label(year):
 
 
 def main():
+    load_dotenv()
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
+    path_table = os.getenv('PATH_TABLE', default = 'drinks.xlsx')
     template = env.get_template('template.html')
-    year_of_creation = 1920
-    drinks_in_excel = pandas.read_excel('drinks.xlsx', keep_default_na=False)
+    drinks_in_excel = pandas.read_excel(path_table, keep_default_na=False)
     all_drinks = drinks_in_excel.to_dict(orient='records')
     sorted_categories = get_sorting_categories(all_drinks)
     rendered_page = template.render(
-        old=get_age_label(get_an_age(year_of_creation)),
-        sorted_categories=sorted_categories,
-        titles_drinks=get_category_titles(sorted_categories)
-    )
+        age=get_age_label(get_an_age(CREATION_YEAR)),
+        sorted_categories=sorted_categories.items(),
+        )
 
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
@@ -65,4 +64,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
